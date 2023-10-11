6b9597af79a7654c58a60231fd23232c85f3a516
Gautier RICHARD <26085589+gtrichard@users.noreply.github.com>
2023-10-11 15:23:04 +0200

    Update spread3_rendering_fixer.sh

--- spread3_rendering_fixer.sh
+++ spread3_rendering_fixer.sh
@@ -3,6 +3,45 @@
 #Source of the script: https://github.com/phylogeography/SpreaD3/issues/32#issuecomment-540692525
 
 
+# Define usage function
+usage() {
+    echo " Fixes SpreaD3 output for recent Web Browsers, by Terry Cojones, 06/12/2019.
+ Usage: $0 
+    [ -i ] SpreaD3 Output Folder Name. Example: -i path/to/Spread3_output/renderers/d3/d3renderer
+    [ -h ] Prints help."
+}
+
+
+# Parse arguments with getopt
+while getopts "i::h" opt; do
+  case $opt in
+    i)
+      arg_i="${OPTARG}"
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+# Check if arg_i is empty
+if [ -z "$arg_i" ]; then
+  echo "[ ERROR  ] No input SpreaD3 folder provided."
+  usage
+  exit 1
+fi
+
 set -Eeuo pipefail
 
 (
